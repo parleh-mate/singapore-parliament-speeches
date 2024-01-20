475b619659d46cf43d254d7f4486b857ff91c43c
Jeremy Chia <jeremyjchia@gmail.com>
2024-01-20 23:35:01 +0100

    refactor: change fields

--- scripts/load/sittings.py
+++ scripts/load/sittings.py
@@ -6,8 +6,8 @@ def dataframe(metadata):
     sitting_df = pd.DataFrame(
         {
             "Sitting_CID": [sittings.cid(metadata)],
-            "Sitting_Date": [sittings.datetime_str(metadata)],
-            "Parliament Session Str": [metadata["partSessionStr"]],
+            "Sitting_Date": [sittings.date_str(metadata)],
+            "Sitting_DateTime": [sittings.datetime_str(metadata)],
             "Parliament_Number": [metadata["parlimentNO"]],
             "Session_Number": [metadata["sessionNO"]],
             "Volume_Number": [metadata["volumeNO"]],
