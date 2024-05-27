a8703e1fe41cd8b55a99d0ad59444e1e08cdf96a
jeremychia <jeremy.chia@vinted.com>
2024-05-27 22:11:46 +0200

    chore: declare regex=True

--- scripts/transform/speeches.py
+++ scripts/transform/speeches.py
@@ -20,7 +20,7 @@ def clean_rows(temp_df):
     temp_df["Text"] = temp_df["Text"].str.replace("proc text", "", case=False)
 
     # 3. Remove "pages" from header
-    temp_df["Text"] = temp_df["Text"].str.replace(r"Page  \d+", "")
+    temp_df["Text"] = temp_df["Text"].str.replace(r"Page  \d+", "", regex=True)
 
     # 4. Remove "None" from header
     temp_df["Text"] = temp_df["Text"].str.replace("None", "")
